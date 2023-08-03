29a633021969dd95590c5c1bd446117fdf6ecd2c
aakarkun <aakar.kun@hotmail.com>
2023-08-03 18:45:08 +1000

    Feed implemented in Page component

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,18 @@
+import Feed from '@components/Feed';
+
+
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
         Discover & Share
         <br className="max-md:hidden" />
-        <span className="orange_gradient text-center">AI Powered Prompts</span>
+        <span className="orange_gradient text-center"> AI Powered Prompts</span>
       </h1>
         <p className="desc text-center">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae ea eum fuga explicabo ullam magni exercitationem eius eligendi, dolorum cumque soluta dolorem quo aliquid amet, quam provident suscipit optio saepe.
         </p>
-        {/* Feed */}
+        <Feed />
     </section>
   );
 };
