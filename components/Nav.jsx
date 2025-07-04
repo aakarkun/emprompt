cf9e36a05d0e3f2fdf1eadb897933deae0d680ca
aakarkun <aakar.kun@hotmail.com>
2025-07-04 11:34:13 +1000

    Nav and package update

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,20 +6,20 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const { data: session } = useSession();
+  // const { data: session } = useSession();
 
-  const [providers, setProviders] = useState(null);
-  const [toggleDropdown, setToggleDropdown] = useState(false);
+  // const [providers, setProviders] = useState(null);
+  // const [toggleDropdown, setToggleDropdown] = useState(false);
 
-  useEffect(() => {
-    const setUpProviders = async () => {
-      const response = await getProviders();
+  // useEffect(() => {
+  //   const setUpProviders = async () => {
+  //     const response = await getProviders();
 
-      setProviders(response);
-    };
+  //     setProviders(response);
+  //   };
 
-    setUpProviders();
-  }, []);
+  //   setUpProviders();
+  // }, []);
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
